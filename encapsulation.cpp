36fffdae38cbe264be12a1b604e943ad3cc74fb0
akmaalsyh <taufansyah50@gmail.com>
2024-05-28 15:48:01 +0700

    memasukkan int main dan memanggil remote lampu, setsaklarno, dan getsaklarno

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -16,3 +16,20 @@ public:
         return saklarNo[i];
     }
 };
+
+int main()
+{
+    remoteLampu LampuRumah;
+
+    LampuRumah.setsaklarNo(0, "Lampu Teras Rumah");
+    LampuRumah.setsaklarNo(1, "Lampu Ruang Tamu");
+    LampuRumah.setsaklarNo(2, "Lampu Kamar Tidur");
+    LampuRumah.setsaklarNo(3, "Lampu Dapur");
+
+    cout << LampuRumah.getsaklarNo(0) << endl;
+    cout << LampuRumah.getsaklarNo(1) << endl;
+    cout << LampuRumah.getsaklarNo(2) << endl;
+    cout << LampuRumah.getsaklarNo(3) << endl;
+
+    return 0;
+}
