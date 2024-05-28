804cfb91e03d6d4cd7be9a98d77650ecf4c03484
akmaalsyh <taufansyah50@gmail.com>
2024-05-28 15:44:35 +0700

    memasukkan class remote lampu

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+class remoteLampu
+{
+private:
+    string saklarNo[10];
+public:
+    void setsaklarNo(int i, string value)
+    {
+        saklarNo[i] = value;
+    }
+
+    string getsaklarNo(int i)
+    {
+        return saklarNo[i];
+    }
+};
